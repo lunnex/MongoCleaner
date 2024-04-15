ab78b6e46151e383609c8e33e9980794eac8691e
CORP\ik.korotkikh <ik.korotkikh@abonentplus.ru>
2024-04-15 13:55:00 +0300

    refactoring + account deletion

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ import re
 import pyodbc
 
 client = pymongo.MongoClient('mongodb://admin:PR2G1Vz6M7VO7ZEHKzL0JXKg4v8ryJ1b@192.168.51.14:27017/admin')
-year = 12
-month = 11
 
 connection = pyodbc.connect("Driver={SQL Server Native Client 11.0};"
                         "Server=192.168.51.11;"
@@ -12,55 +10,67 @@ connection = pyodbc.connect("Driver={SQL Server Native Client 11.0};"
                         "uid=UserAlpha;pwd=ALSUtCF%utnWJk6Oa~RI?#q0v!")
 
 buffer = dict()
-def clean(dbName):
-    db = client[dbName]
+def clean(eps):
+    db = client[eps]
     accounts = db["Account"]
-    accountCount = accounts.count_documents({})
+    account_count = accounts.count_documents({})
     accounts = accounts.find()
-    print("Количество абонентов в платежной системе " + dbName + " " + str(accountCount))
+    print(f"Количество абонентов в платежной системе {eps}: {account_count}")
 
-    i = 0
-    for account in accounts:
-        i += 1
-        if (i % 1000 == 0):
-            print("Обработано " + str(i) + " из " + str(accountCount))
+    for i, account in enumerate(accounts):
+        account_id = account["_id"]["Account"]
+        if account_id == "001":
+            continue
 
-        for dp in account["DestParams"]:
-            year = re.search("12=\\d{4}", dp["Value"])
+        for service in account["DestParams"]:
+            year = re.search("12=\\d{4}", service["Value"])
 
-            if(year != None):
-                year = year.group(0)
-            else:
+            if year is None or year.group(0) == "12=2024":
                 continue
+            service_id = service["Id"]
 
-            if year != "12=2024" and account["_id"]["Account"] != "001":
-                DPId = dp["Id"]
+            log_with_service_name(service_id, account, eps, service)
+            delete_services(service_id, account, db, eps)
 
-                LoggingWithPDName(DPId, account, dbName, dp)
-                Deletion(DPId, account, db, dbName)
+        if len(account["DestParams"]) == 0:
+            eps_id = account["_id"]["EPSId"]
+            delete_account(db, account_id, eps_id)
+
+        if i % 1000 == 0:
+            print(f"Обработано {i} из {account_count}")
 
 
-def Deletion(DPId, account, db, dbName):
+def delete_services(service_id, account, db, service_name):
     acc = account["_id"]["Account"]
-    eps = dbName.replace("eps_", "")
-    filterQuery = {"_id.Account": acc, "_id.EPSId": int(eps)}
-    updateQuery = {"$pull": {"DestParams": {"Id": int(DPId)}}}
-    db.Account.update_one(filterQuery, updateQuery)
+    eps = service_name.replace("eps_", "")
+    filter_query = {"_id.Account": acc, "_id.EPSId": int(eps)}
+    update_query = {"$pull": {"DestParams": {"Id": int(service_id)}}}
+    db.Account.update_one(filter_query, update_query)
 
 
-def LoggingWithPDName(DPId, account, dbName, dp):
-    if buffer.get(DPId) == None:
-        query = f"SELECT PaymentDestinationName from PaymentDestinations where PaymentDestinationID = {DPId}"
-        pdName = str(connection.execute(query).fetchone()[0])
-        buffer[DPId] = pdName
-    pdName = buffer[DPId]
+def delete_account(db, account, eps):
+    db.Account.delete_one({"_id.Account": account, "_id.EPSId": int(eps)})
+
+
+def log_with_service_name(service_id, account, eps, service_params):
+    get_service_name(service_id)
+
+    service_name = buffer[service_id]
     acc = account["_id"]["Account"]
-    params = dp["Value"][:13]
+    params = service_params["Value"][:13]
     with open("E:\\DeletedRows.txt", "a") as file:
-        input = "Account: " + acc + " " + dbName + " PD: " + str(DPId) + " " + "PDName: " + pdName + " " + params + "\n"
-        file.write(input)
+        line = f"Account: {acc} {eps} PD: {service_id} PDName: {service_name} {params} \n"
+        file.write(line)
+
+
+def get_service_name(service_id):
+    service_name = buffer.get(service_id)
+    if service_name is None:
+        query = f"SELECT PaymentDestinationName from PaymentDestinations where PaymentDestinationID = {service_id}"
+        service_name = str(connection.execute(query).fetchone()[0])
+        buffer[service_id] = service_name
 
 
 dbs = client.list_database_names()[2:]
-for dbName in dbs:
-    clean(dbName)
+for db_name in dbs:
+    clean(db_name)
